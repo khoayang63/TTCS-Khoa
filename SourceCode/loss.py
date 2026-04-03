9b6504cb67846450a93e21e837a3be2bd542fc9f
khoayang63 <khoayang0603@gmail.com>
2026-04-04 01:19:45 +0700

    add loss.py

--- SourceCode/loss.py
+++ SourceCode/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from utils import *
 import config
 from dataset import get_train_test_loader
@@ -83,6 +84,136 @@ def build_targets(targets, anchors, S, num_anchors_per_scale=3, ignore_iou_thres
 
     return yolo_targets
 
+
+# FL=−α(1−p)γlog(p)
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.bce = nn.BCEWithLogitsLoss(reduction="none")
+
+    def forward(self, preds, targets):
+        bce_loss = self.bce(preds, targets)
+        probs = torch.sigmoid(preds)
+
+        pt = targets * probs + (1 - targets) * (1 - probs)
+
+        alpha_factor = targets * self.alpha + (1 - targets) * (1 - self.alpha)
+
+        focal_weight = alpha_factor * (1 - pt) ** self.gamma
+
+        return (focal_weight * bce_loss).mean()
+
+
+class QFocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.bce = nn.BCEWithLogitsLoss(reduction="none")
+
+    def forward(self, preds, targets):
+        # targets: IoU (0 → 1)
+
+        bce_loss = self.bce(preds, targets)
+        probs = torch.sigmoid(preds)
+        alpha_factor = targets * self.alpha + (1 - targets) * (1 - self.alpha)
+
+        focal_weight = alpha_factor * torch.abs(targets - probs) ** self.gamma
+
+        return (focal_weight * bce_loss).mean()
+    
+
+class YOLOLoss(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.bce = nn.BCEWithLogitsLoss()
+        self.entropy = nn.CrossEntropyLoss()
+        self.lambda_box = 5.0
+        self.lambda_noobj = 0.5
+
+        self.anchors = torch.tensor(config.ANCHORS, device = config.DEVICE) # Normalize
+        self.S = config.S # [13, 26, 52]
+
+    def forward(self, predictions, targets):
+        # yolo_targets = build_targets(targets, self.anchors, self.S) 
+
+        loss_obj = 0
+        loss_noobj = 0
+        loss_box = 0
+        loss_class = 0
+
+        for i, (pred, target) in enumerate(zip(predictions, targets)):
+            # target(B, 3, S, S, 6)
+            # predictions (B, 3, S, S, 5 + num_classes)
+
+            obj = target[..., 0] == 1
+            noobj = target[..., 0] == 0
+
+            # noobj loss
+            loss_noobj += self.bce( #ok
+                pred[..., 0][noobj], target[..., 0][noobj]
+            )
+            if obj.any():
+                # obj loss 
+                # (x_off y_off, h_pred, w_pred)
+            
+                anchors = (self.anchors[i] * self.S[i]).reshape(1, 3, 1, 1, 2) # grid space
+
+                # offset trong ô + anchor wh
+                box_preds = torch.cat([
+                    torch.sigmoid(pred[..., 1:3]), 
+                    torch.exp(pred[..., 3:5].clamp(max=10)) * anchors
+                ], dim=-1)
+
+                ious = intersection_over_union(
+                    box_preds[obj],
+                    target[..., 1:5][obj]
+                ).detach() #(B, 3, S, S,)
+
+                # print(ious.shape, pred[..., 0][obj].shape)
+                loss_obj += self.bce( #ok
+                    pred[..., 0][obj], ious
+                )
+
+                # box loss
+                xy = torch.sigmoid(pred[..., 1:3])
+                wh = pred[..., 3:5]
+                
+                target_xy = target[..., 1:3]
+
+                target_wh = torch.log(
+                    (target[..., 3:5] / anchors) + 1e-7
+                )
+                
+                pred_box = torch.cat([xy, wh], dim=-1)
+                target_box = torch.cat([target_xy, target_wh], dim=-1)
+                
+                
+                loss_box += self.mse( #ok
+                    pred_box[obj],
+                    target_box[obj]
+                )
+                # classification loss
+                # loss_class += self.entropy( #ok
+                #     pred[..., 5:][obj],
+                #     target[..., 5][obj].long()
+                # )
+                num_classes = pred.shape[-1] - 5
+
+                target_class = target[..., 5][obj].long()   # (N,)
+                target_onehot = F.one_hot(target_class, num_classes=num_classes).float()  # (N, C)
+                loss_class += self.bce( #ok
+                    pred[..., 5:][obj],
+                    target_onehot
+                )
+
+        return (self.lambda_box * loss_box + 
+                self.lambda_noobj * loss_noobj + 
+                loss_obj + loss_class), loss_box, loss_obj, loss_noobj, loss_class
+        
 
 
 def count_anchor_states(train_loader):
